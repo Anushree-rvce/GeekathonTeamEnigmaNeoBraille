600110b374c06ce3bfdbe34a3bda975bec769a6e
Anushree-rvce <anushreess05@gmail.com>
2018-01-07 09:14:36 +0530

    Delete Announce.py

--- Announce.py
+++
@@ -1,8 +0,0 @@
-from os import startfile
-from time import sleep
-if __name__=="__main__":
-    startfile("first.mp3")
-    sleep(25)
-    startfile("second.mp3")
-    sleep(30)
-    startfile("third.mp3")
